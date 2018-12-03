9477cdfae945a04353788bd6320aa9adc436fefd
MrEd <macbookpro@macbooks-mbp.boisestate.edu>
2018-12-02 19:47:02 -0700

    Second

--- R/url2file.R
+++ R/url2file.R
@@ -1,12 +1,13 @@
-#' url2file
-#' This function downloads a file from a web address and places it in a file in your working directory.
-#' Arguments:
-#' urlFile: provide a url address pointing to the file to download stored in a vector
-#' dirID: name of the directory to save files in (default = "Downloaded_files");
+#' @title Download a file from a URL into your working directory
+#' @description This is a fuction to streamline downloading files from a URL to a new or existing folder in the working directory.
+#' @param  urlfile provide a URL adddress in quotes of the file to download stored in a vector
+#' @param dirID provide the name of an existing or new folder (default = "Downloaded_files")
+#' @examples  By executing "url2file("https://page.com")" will download the page into a file called "Downloaded_files" in your working directory.
+#' @examples  You can specify an existing folder in the working directory (dirID="myfolder") or specify a new folder (dirID="newfolder")
 #' @export
-url2file <- function(urlFile, dirID = "Downloaded_files"){
-  fileName <- strsplit(urlFile, split="/")[[1]]
+url2file <- function(urlfile, dirID = "Downloaded_files"){
+  fileName <- strsplit(urlfile, split="/")[[1]]
   fileName <- fileName[length(fileName)]
   if(!dir.exists(dirID)) dir.create(dirID)
-  download.file(urlFile, destfile = paste(dirID, "/", fileName, sep=''))
+  download.file(urlfile, destfile = paste(dirID, "/", fileName, sep=''))
 }
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 library(testthat)
-library(ffurl)
+library(url2file)
 
-test_check("ffurl")
+test_check("url2file")
